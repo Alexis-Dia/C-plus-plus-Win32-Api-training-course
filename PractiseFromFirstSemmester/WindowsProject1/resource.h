7d339f2b6526469e31d6029c7565b43ea5bccf60
Alexis-Dk <alexeydruzik@inbox.ru>
2020-01-29 10:01:45 +0300

    Fixed structure.

--- PractiseFromFirstSemmester/WindowsProject1/resource.h
+++ PractiseFromFirstSemmester/WindowsProject1/resource.h
@@ -24,6 +24,7 @@
 #define ID_THREAD_CREATETHREAD          32776
 #define ID_SETEDITANDLIST_CHANGEEDIT    32777
 #define ID_SETEDITANDLIST_CHANGELIST    32778
+#define ID_CHANGEWINDOWTITLE_CHANGE     32779
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -32,7 +33,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        130
-#define _APS_NEXT_COMMAND_VALUE         32779
+#define _APS_NEXT_COMMAND_VALUE         32780
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
